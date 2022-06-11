faae43bdc8fcbf2bae753623d727a0cd2325df4b
ddamjanovic <damjanovic27@gmail.com>
2022-06-11 13:31:14 +0200

    Add all

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import AutoComplete from './components/AutoComplete';
+import logo from "./logo.svg";
+import "./App.css";
+import AutoComplete from "./components/AutoComplete";
 
 function App() {
   return (
